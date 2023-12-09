e74bcd15c1fdb17f1a3d2347534370259cd8b97d
hxzd5568 <40557101+hxzd5568@users.noreply.github.com>
2023-12-09 20:14:19 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,15 @@
+# base module architecture
+# -----------
+#
+# -base_utils.py
+#     -checkor
+#         random_difference_test
+#     -fuzzer.py
+#         detect error-triggering inputs
+#     -errortrace.py
+#         match nodes and calculate errors
+#     -propainfo.py
+#         error accumulation graph generation
+#     -fix_utils.py
+#         autofix the errors
+
